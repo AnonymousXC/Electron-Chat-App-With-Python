95706c09f78c22c4145b0fc00e0138f96d72789f
Falconic-coder <mohammad.ahmad852@yahoo.com>
2022-05-10 10:57:37 +0530

    content fetched for news section main page

--- src/python/api/news.py
+++ src/python/api/news.py
@@ -1,27 +1,45 @@
 import requests
 import json
 
+page = 0
 
 def article_search(api_key, search="World"):
-
-    parameter = {"api-key": api_key, "q": search}
+    
+    parameter = {"page": page, "api-key": api_key, "q": search}
     data = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", params=parameter)
     return data.json()["response"]["docs"]
 
 
 def content(data):
+    website = "https://www.nytimes.com/"
+    author_name = []
+    lead_paragraph = []
     abstract = []
     web_url = []
     image_url = []
-    lead_paragraph = []
+    main_headline = []
+    
 
     for i in range(10):
         abstract.append(data[i]["abstract"])
         web_url.append(data[i]["web_url"])
         lead_paragraph.append(data[i]["lead_paragraph"])
-        image_url.append("https://www.nytimes.com/"+data[i]["multimedia"][0]["url"])
+        main_headline.append(data[i]["headline"]["main"])
+        author_name.append(data[i]["byline"]["original"])
 
-    return abstract, web_url, image_url, lead_paragraph
+        try:
+            image_url.append(website+data[i]["multimedia"][0]["url"])
+        except IndexError:
+            image_url.append("")
+
+    return abstract, main_headline ,web_url, image_url, author_name
+
+
+def load_article(api_key, search="World"):
+    global page
+    page += 1
+    data = article_search(api_key=api_key, search=search)
+    return content(data)
 
 
 if __name__ == "__main__":
