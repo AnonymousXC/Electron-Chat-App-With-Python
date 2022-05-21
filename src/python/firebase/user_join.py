bbfdd4f5710c546e9776630291b93fbe5637da52
Falconic-coder <mohammad.ahmad852@yahoo.com>
2022-05-21 12:51:55 +0530

    added message count and number of users

--- src/python/firebase/user_join.py
+++ src/python/firebase/user_join.py
@@ -11,30 +11,52 @@ firebase_admin.initialize_app(cred,{
     "databaseURL" : link
 })
 
-ref = db.reference("User")
+ref = db.reference("Users")
 
 
-def user_join(username, password, timestamp):
+def user_join(username, timestamp, msg=False):
+
+    if not msg:
+        ref.update(
+            {
+                username:
+                {
+                    "timestamp" : timestamp,
+                    "messages" : 0
+                }
+                
+            }
+        )
+    
+    else:
+        update_msg_count(username)
+
+
+def update_msg_count(username):
+    new_ref = db.reference(f"Users/{username}")
+    msgs = new_ref.get()["messages"]
 
     ref.update(
         {
-            
-                username:
-                {
-                    "username" : username,
-                    "password": password,
-                    "timestamp" : timestamp
-                }
-            
-
+           "messages" : msgs+1
         }
     )
 
 
+def number_of_user():
+    return len(ref.get())
+
+
 if __name__ == "__main__":
     username = sys.argv[1]
-    password = sys.argv[2]
-    timestamp = sys.argv[3]
+    timestamp = sys.argv[2]
+    msg_bool = sys.argv[3]
 
-    if username != "" and password != "" and timestamp != "":
-        user_join(username, password, timestamp)
+    if msg_bool == "":
+        msg_bool = False
+    else:
+        msg_bool = True
+
+    if username != ""  and timestamp != "":
+        user_join(username, timestamp, msg_bool)
+        print(number_of_user())
