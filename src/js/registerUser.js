95b71c936e13bc1081b19a72443be1350a1d9756
AnonymousXC <cvxanonymous007@gmail.com>
2022-05-22 22:49:49 +0530

    Made minor Changes

--- src/js/registerUser.js
+++ src/js/registerUser.js
@@ -6,6 +6,14 @@ function registerUser() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
+    if(username == "" || email == "" || password == "" || !username || !email || !password)
+    {
+        document.getElementById("status").innerHTML = "Please fill all the fields."
+        return
+    }
+
+    document.getElementById("status").innerHTML = `Processing <img src="../resources/gifs/cupertino_activity_indicator.gif" alt="" width="23px" id="loading-gif">`
+
     let path = "src/python/firebase/registration.py"
     let options = {
         mode: 'text',
@@ -15,8 +23,10 @@ function registerUser() {
 }
 
 window.onmessage = async (e) => {
-    if(e.source !== window) return;
+    if(e.source !== window || !e.data) return;
     let lent = e.data.length - 1;
-    if(e.data[lent] == "registration.py")
+    if(e.data[lent] == "registration.py") 
+    {
         document.getElementById("status").innerText = e.data[0]
+    }
 }
